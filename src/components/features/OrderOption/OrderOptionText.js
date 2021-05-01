68e8954e620212ee8d37803b87176eaa63ca2815
Sebastian Sikora <ssikora80@gmail.com>
2021-05-01 14:56:11 +0200

    Add OrderOptionText and OrderOptionDate

---
+++ src/components/features/OrderOption/OrderOptionText.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './OrderOption.scss';
+
+const OrderOptionText = () => (
+  <div>
+    <input
+      type='text'
+      className={styles.input}
+    />
+  </div>
+);
+
+export default OrderOptionText;
