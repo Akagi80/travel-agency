eeb6ef4772d54a16aa08c3f2b5d4111435a4ae60
Sebastian Sikora <ssikora80@gmail.com>
2021-06-01 19:52:59 +0200

    Add formatTime base on tests

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,1 +1,12 @@
-export const formatTime = () => 'formatted time';
+export const formatTime = arg => {
+  if (!arg || typeof(arg) !== 'number' || arg < 0) {
+    return null;
+  }
+
+  const sec = Math.floor(arg % 60);
+  const min = Math.floor(arg / 60 % 60);
+  const hrs = Math.floor(arg / 3600);
+
+  const displayCountdown = [hrs, min, sec].map(element => `${element + 100}`.substring(1));
+  return displayCountdown.join(':');
+};
